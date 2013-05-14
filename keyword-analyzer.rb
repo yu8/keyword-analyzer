0f24bb9ddda30dcc17be25cbbae084e645bd5329
yu8 <s.tyaro@gmail.com>
2013-05-14 23:24:36 +0900

    keyword-analyzer

---
+++ keyword-analyzer.rb
@@ -0,0 +1,39 @@
+# encoding: utf-8
+
+require 'csv'
+require 'MeCab'
+
+
+file = './20130501.csv'
+
+data = CSV.read(file)
+
+#data.each do |data|
+#  puts data[0]
+#end
+
+
+mecab = MeCab::Tagger.new()
+
+n_data = Array.new
+
+i = 0
+data.each do |data|
+  #puts mecab.parse(data[0])
+  
+  #n_data[i].push(mecab.parseToNode(data[0]))
+  node = mecab.parseToNode(data[0])
+  while node do
+    n_data[i] = Array.new
+    n_data[i].push(node)
+    n_data[i].push(data[1])
+    n_data[i].push(data[2])
+    i += 1
+    node = node.next
+  end
+end
+
+puts n_data[0][0]
+puts n_data[0][1]
+puts n_data[0][2]
+
